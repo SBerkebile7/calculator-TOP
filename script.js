34e65bbf0b55102c104f7ce186634dc2c54c48c7
sberkebile7 <sberkebile@live.com>
2025-12-04 16:52:44 -0800

    working on functions now

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-const calcContainer = document.querySelector(".calcContainer");
+const calcContainer = document.querySelector('.calcContainer');
+const calcBtns = document.querySelectorAll('.btn');
+
+let firstNum = '';
+let lastNum = '';
+console.log("calcBtns ", calcBtns);
+
+calcBtns.forEach((btn) => { 
+    const btnValue = btn.innerText;
+    
+    console.log("btnValue ", btnValue);
+    btn.onclick = () => {
+        // perform math function(btnValue);
+    };
+});
 
 const add = function(array) {
-	 return array.reduce((x, y) => x + y);
+	return array.reduce((x, y) => x + y);
 };
 
 const subtract = function(array) {
